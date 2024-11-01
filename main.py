1550f60ca51ad165bcfddedc967c73950660954a
WilliamsAl282 <WilliamsAl282@northwested.org>
2024-11-01 10:25:23 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+# Alexander Williams
+# 11/1/2024
+# Web and App Development
+
+# 4-3 Counting to Twenty
+
+#for i in range(1,21):
+    #print(i)
+
+#4-4 One Million
+    
+#for i in range(1,100000000):
+    #print(i)
+
+# 4-5 Summing a Million
+    
+# print(min(range(1,1000000000)))
+# print(max(range(1,1000000001)))
+# print(sum(range(1,1000000001)))
+
+# 4-6 Odd Numbers
+
+for i in range(1,21,2):
+    print(i)
+
+# 4-7 Threes
+ 
+for i in range(3,31,3):
+    multiples = i / 3
+    print(f'{int(multiples)} and 3 are the multiples of {i}')
+
+# 4-8
+
+for i in range(1,11):
+    cubed = i**3
+    print(f'{i} cubed is {int(cubed)}')
